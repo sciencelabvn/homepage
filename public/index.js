98eeaaf7c7226d5f35e4282e6335bfdc5af99044
Trang Nguyen <92909404+overthinkingchang@users.noreply.github.com>
2023-08-27 21:44:54 +0200

    site: initial wmr

---
+++ public/index.js
@@ -0,0 +1,37 @@
+import {
+  ErrorBoundary,
+  LocationProvider,
+  Route,
+  Router,
+  hydrate,
+  lazy,
+  prerender as ssr,
+} from "preact-iso"
+import Header from "./header.js"
+import NotFound from "./pages/_404.js"
+import Home from "./pages/home/index.js"
+
+const About = lazy(() => import("./pages/about/index.js"))
+
+export function App() {
+  return (
+    <LocationProvider>
+      <div class="app">
+        <Header />
+        <ErrorBoundary>
+          <Router>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route default component={NotFound} />
+          </Router>
+        </ErrorBoundary>
+      </div>
+    </LocationProvider>
+  )
+}
+
+hydrate(<App />)
+
+export async function prerender(data) {
+  return await ssr(<App {...data} />)
+}
